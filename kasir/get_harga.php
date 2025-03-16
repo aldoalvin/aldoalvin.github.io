7e3e0b4c702d461958d99fe29444a47f26a72626
aldoalvin <118593320+aldoalvin@users.noreply.github.com>
2025-03-16 10:05:24 +0700

    Delete kasir directory

--- kasir/get_harga.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $koneksi = mysqli_connect('localhost','root','','sjt');
-    $nama_barang = $_POST['nama_barang'];
-
-    $query = mysqli_query($koneksi, "SELECT harga_barang FROM tbl_barang WHERE nama_barang = '$nama_barang'");
-    $data = mysqli_fetch_array($query);
-
-    echo $data['harga_barang'];
-?>
